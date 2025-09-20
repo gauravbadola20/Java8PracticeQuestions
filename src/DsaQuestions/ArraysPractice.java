4bd05a02990f70269944ac5a0abf305d4c8cc07e
SpringTwofactorAuthentication <gauravbadola20@gmail.com>
2025-09-20 11:36:32 -0700

    new commit

--- src/DsaQuestions/ArraysPractice.java
+++ src/DsaQuestions/ArraysPractice.java
@@ -23,19 +23,28 @@ public class ArraysPractice {
         }
     }
 
+    // linear serach
+    public static int linearSearch(int arr[], int num){
+
+        for (int i = 0; i < arr.length; i++){
+
+            if (arr[i] == num){
+                return i;
+            }
+        }
+        return  -1;
+    }
+
 
 
     public static void main(String[] args) {
 
 
-        int arr[] = {1,0,2,3,0,4,0,1};
+        int arr[] = {1,2,3,4,5};
+        int num = 3;
 
-        int n = arr.length;
+       System.out.println( linearSearch(arr,num));
 
-        putZerosAtLast(arr,n);
 
-        for (int i : arr){
-            System.out.print(i+" ");
-        }
     }
 }
