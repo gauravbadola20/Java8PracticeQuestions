8c6c9bf123d68bce35a728d1d611f3e8aeaf0f90
SpringTwofactorAuthentication <gauravbadola20@gmail.com>
2025-10-01 02:13:21 -0700

    new commit

--- src/JavaMedium/PracticeMedium.java
+++ src/JavaMedium/PracticeMedium.java
@@ -79,11 +79,42 @@ public class PracticeMedium {
         }
     }
 
+
+    // optimal approach
+    public static void sortArrays012s(int arr[], int n){
+
+        int low = 0; int mid  = 0;
+        int high = n-1;
+
+        while (mid <= high){
+
+            if (arr[mid] == 0){
+
+                //swap mid with low
+                int temp = arr[mid];
+                arr[mid] = arr[low];
+                arr[low] = temp;
+                low++;
+                mid++;
+            } else if (arr[mid] == 1) {
+                mid++;
+
+            }else {
+                //swap with mid with high
+
+                int temp  = arr[mid];
+                arr[mid]  = arr[high];
+                arr[high] = temp;
+                high--;
+            }
+        }
+    }
+
     public static void main(String[] args) {
 
         int[] arr = {0, 2, 1, 2, 0, 1, 2, 0};
         int n = arr.length;
-        sortZerosOnesTwos(arr,n);
+        sortArrays012s(arr,n);
 
         System.out.print("Sorted Array: ");
         for (int num : arr) {
